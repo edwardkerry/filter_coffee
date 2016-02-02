be650a09b268c2f7d26ca4b88e5626fcc0b3a3f7
Daniela <daniela.galeote@gmail.com>
2016-02-02 15:58:59 +0000

    User can add a workspace

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,6 +1,19 @@
 class WorkspacesController < ApplicationController
 
   def index
-    
   end
+
+  def new
+    @workspace = Workspace.new
+  end
+
+  def create
+    @workspace = Workspace.create(workspace_params)
+    redirect_to "/workspaces"
+  end
+
+  def workspace_params
+    params.require(:workspace).permit(:name)
+  end
+
 end
--- spec/features/workspace_feature_spec.rb
+++ spec/features/workspace_feature_spec.rb
@@ -5,7 +5,7 @@ feature "work spaces" do
     scenario "it should display a prompt" do
       visit "/workspaces"
       expect(page).to have_content "No workspaces added"
-      expect(page).to have_button "Add a workspace"
+      expect(page).to have_link "Add a workspace"
     end
   end
 
@@ -20,5 +20,16 @@ feature "work spaces" do
       expect(page).not_to have_content "No workspaces added"
     end
   end
+
+  context "creating workspaces" do
+    scenario "prompts user to fill out a form to add a new workspace, then displays it" do
+    visit "/workspaces"
+    click_link "Add a workspace"
+    fill_in "Name", with: "SecondHome"
+    click_button "Create Workspace"
+    expect(page).to have_content "SecondHome"
+    expect(current_path).to eq "/workspaces"
+  end
+end
 
 end
