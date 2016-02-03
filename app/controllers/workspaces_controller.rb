e3da686202de68b42f71331686a863e4a509b6ed
edwardkerry <edwardkerry@gmail.com>
2016-02-03 21:14:18 +0000

    WIP json implementation

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class WorkspacesController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
 
@@ -7,13 +9,31 @@ class WorkspacesController < ApplicationController
 
   def new
     @workspace = Workspace.new
-    @client = GooglePlaces::Client.new('AIzaSyAkk0WzkJl4oPNEYm6YbpXasQHRfHB3zSk')
-    @spots = @client.spots(51.517912, -0.0737660)
+
   end
 
   def create
-    @workspace = Workspace.new(workspace_params)
-    if @workspace.save
+    @workspace = Workspace.new(name: workspace_params[:name].split(",")[0], address: workspace_params.split(",").shift)
+
+    # @client = GooglePlaces::Client.new('AIzaSyAkk0WzkJl4oPNEYm6YbpXasQHRfHB3zSk')
+    # @spot = @client.spot("#{@workspace.latitude},#{@workspace.longitude}")
+
+      if @workspace.save
+        get_place_id(@workspace)
+
+
+
+
+
+    # url = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?name=#{workspace_params[:name]}&types=establishment&key=AIzaSyAkk0WzkJl4oPNEYm6YbpXasQHRfHB3zSk")
+    # http = Net::HTTP.new(url.host, url.port)
+    # http.use_ssl = true
+    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
+    # request = Net::HTTP::Get.new(url.request_uri)
+    # res = http.request(request)
+    # puts res.body
+
+
       redirect_to "/workspaces"
     else
       render "new"
@@ -46,4 +66,13 @@ class WorkspacesController < ApplicationController
     params.require(:workspace).permit(:name, :address, :latitude, :longitude)
   end
 
+  def get_place_id(workspace)
+    url = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{workspace.latitude},#{workspace.longitude}&rankby=distance&name=#{workspace.name}&types=establishment&key=AIzaSyAkk0WzkJl4oPNEYm6YbpXasQHRfHB3zSk")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
+    request = Net::HTTP::Get.new(url.request_uri)
+    res = http.request(request)
+    puts res.body
+  end
 end
